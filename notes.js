f935634a7c1312f993cfcdbf4bd442323e69083e
iseefirehy_hongyu <zhanghongyu2011@gmail.com>
2018-03-27 14:05:13 -0700

    Start the app with the filesystem

--- notes.js
+++ notes.js
@@ -1,12 +1,20 @@
 console.log('notes.js');
 
-module.exports.age = 25;
-
-module.exports.addNote = () =>{
-  console.log('AddNote');
-  return 'New Note';
+var addNote = (title,body) => {
+    console.log('Adding note',title,body);
 };
 
-module.exports.add = (a,b) =>{
-  return a + b;
+var getAll = () =>{
+  console.log('Getting all notes');
 };
+
+var getNote = (title) => {
+    console.log('Getting note',title);
+};
+var removeNote = (title)=>{
+    console.log('Removing note',title);
+};
+module.exports = {
+  addNote,getAll,getNote,removeNote
+};
+
